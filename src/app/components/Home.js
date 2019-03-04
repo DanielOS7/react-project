f9266f51b870ac3f6b2887efba60e35552b82eaa
Daniel <d.olugbade@outlook.com>
2019-03-04 13:35:21 +0000

    Passing Data with Props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,9 +2,21 @@ import React from "react"
 
 export class Home extends React.Component {
     render() {
+        console.log(this.props);
+        var text = "Something!";
         return (
             <div>
                 <p>In a new Component!</p>
+                <p>{text}</p>
+                <p>Your Name is {this.props.name}, Your age is {this.props.age}</p>
+                <h4>User 1</h4>
+                <h6>Name: {this.props.user.name}</h6>
+                <div>
+                    <h6>Hobbies:</h6>
+                    <ul>
+
+                    </ul>
+                </div>
             </div>
         );
     }
--- src/app/index.js
+++ src/app/index.js
@@ -6,16 +6,20 @@ import { Home } from "./components/Home";
 
 class App extends React.Component {
     render() {
+        var user = {
+            name: "Anna",
+            Hobbies: ["Sports"]
+        }
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Header />
+                        <Header/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home />
+                        <Home name={"Max"} age={27} user={user} />
                     </div>
                 </div>
             </div>
